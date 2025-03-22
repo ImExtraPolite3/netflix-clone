a5b7ad1cda5fd62a3c32e61d9af6e89500526315
ExtraPolite <imextrapolite2@gmail.com>
2025-03-22 16:24:18 -0400

    add conntent for error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,3 +1,23 @@
+import './error.css';
+
 export default function Error() {
-  return <h1>Error</h1>;
+  return (
+    <div id="error-page">
+      <nav>
+        <img src="/netflix-logo.png" alt="" />
+      </nav>
+
+      <div className="content">
+        <h1>Lost your way?</h1>
+        <p>
+          Sorry, we can't find that page. You'll find lots to explore on the
+          home page
+        </p>
+        <button>Netfilx Home</button>
+        <p>
+          Error Code <span>NSES-404</span>
+        </p>
+      </div>
+    </div>
+  );
 }
