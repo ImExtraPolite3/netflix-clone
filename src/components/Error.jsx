3dff0885483fe3d6925b1c01b43f938cf9f23c3d
ExtraPolite <imextrapolite2@gmail.com>
2025-05-20 16:12:29 -0400

    fix error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,27 +2,29 @@ import './error.css';
 
 export default function Error() {
   return (
-    <div id="error-page">
-      <nav id="error-nav">
-        <a href="/profiles">
-          <img src="/netflix-logo.png" alt="" />
-        </a>
-      </nav>
+    <div id="error-container">
+      <div id="error-page">
+        <nav id="error-nav">
+          <a href="/profiles">
+            <img src="/netflix-logo.png" alt="" />
+          </a>
+        </nav>
 
-      <div className="content">
-        <h1>Lost your way?</h1>
-        <p className="error-message">
-          Sorry, we can't find that page. You'll find lots to explore on the
-          home page.
-        </p>
-        <form action="/profiles">
-          <button className="return">Netfilx Home</button>
-        </form>
-        <p className="error-code">
-          Error Code <span>NSES-404</span>
-        </p>
-        <div className="credits">
-          FROM <span>LOST IN SPACE</span>
+        <div className="content">
+          <h1>Lost your way?</h1>
+          <p className="error-message">
+            Sorry, we can't find that page. You'll find lots to explore on the
+            home page.
+          </p>
+          <form action="/profiles">
+            <button className="return">Netflix Home</button>
+          </form>
+          <p className="error-code">
+            Error Code <span>NSES-404</span>
+          </p>
+          <div className="credits">
+            FROM <span>LOST IN SPACE</span>
+          </div>
         </div>
       </div>
     </div>
